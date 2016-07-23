a49e12a9d731523339fca11497fed3399e88da9e
swc8912 <swc8912@gmail.com>
2016-07-23 20:04:46 +0900

    mongoose 모듈 추가, mongodb용 처리로 변경

--- handlers/payday.js
+++ handlers/payday.js
@@ -1,7 +1,42 @@
-var Datastore = require('nedb');
-var db = new Datastore({filename: './data/payday', autoload: true});
+var mongoose = require('mongoose');
 var querystring = require('querystring');
 var url = require('url');
+var db = mongoose.connect('mongodb://localhost/paydaydb');
+var Schema = mongoose.Schema;
+
+var getItemData = new Schema({
+    email: String,
+    itemIdx: Number,
+    date: String
+});
+
+var userData = new Schema({
+    did: String,
+    email: String,
+    money: Number,
+    rank: String,
+    heart: Number,
+    charge: Number,
+    currentBoxId: String,
+    pickItems: String,
+    getPush: String
+});
+
+var logData = new Schema({
+    logCmd: Number,
+    email: String,
+    date: String
+});
+
+var boxdata = new Schema({
+    box: String,
+    bdata: Array
+});
+
+var getItemDataModel = mongoose.model('getItemData', getItemData);
+var userDataModel = mongoose.model('userData', userData);
+var logDataModel = mongoose.model('logData', logData);
+var boxDataModel = mongoose.model('boxdata', boxdata);
 
 exports.create = function(req, res){
 	var body = req.body;
@@ -18,16 +53,6 @@ exports.read = function(req, res){
 		res.json({error: error, results: results});
 	});
 };
-/*
-exports.readUser = function(req, res){
-	var body = req.body;
-	body = typeof body === 'strng' ? JSON.parse(body) : body;
-	console.log("readUser body: " + JSON.stringify(body));
-	//console.log("readUser query: " + req.query.toString());
-	_findPayday(body, function(error, results){
-		res.json({error: error, results: results});
-	});
-}*/
 
 exports.update = function(req, res){
 	var where = req.query;
@@ -51,12 +76,11 @@ function _insertPayday(body, callback){
 	console.log("insertpayday body email: " + body.email);
 	if (body.itemIdx != null) { // 아이템 획득
     	console.log('getItemData');
-        var getItemData = {
-            email: body.email,
-            itemIdx: body.itemIdx,
-            date: body.date
-        }
-        db.insert(getItemData, callback);
+        var data = new getItemDataModel();
+        data.email = body.email;
+        data.itemIdx = body.itemIdx;
+        data.date = body.date;
+        data.save(callback);
     }
 	else if(body.email != null && body.did != null){ // 유저 데이터 추가
 		console.log("insert body: " + body.did);
@@ -66,71 +90,97 @@ function _insertPayday(body, callback){
 		console.log("insert body: " + body.heart);
 		console.log("insert body: " + body.charge);
 		console.log("insert body: " + body.currentBoxId);
-		var userData = {
-			did: body.did,
-			email: body.email,
-			money: body.money,
-			rank: body.rank,
-			heart: body.heart,
-			charge: body.charge,
-			currentBoxId: body.currentBoxId,
-			pickItems: body.pickItems,
-			getPush: body.getPush
-		}
-		db.insert(userData, callback);
+		var data = new userDataModel();
+        data.did = body.did;
+        data.email = body.email;
+        data.money = body.money;
+        data.rank = body.rank;
+        data.heart = body.heart;
+        data.charge = body.charge;
+        data.currentBoxId = body.currentBoxId;
+        data.pickItems = body.pickItems;
+        data.getPush = body.getPush;
+		data.save(callback);
 	}
     else if (body.logCmd != null) { // 로그 데이터 옴
     	console.log('logdata');
-        var logData = {
-            logCmd: body.logCmd,
-            email: body.email,
-            date: body.date
-        }
-        db.insert(logData, callback);
+        var data = new logDataModel();
+        data.logCmd = body.logCmd;
+        data.email = body.email;
+        data.date = body.date;
+        data.save(callback);
     }
     else if(body.boxdata != null){ // 박스 데이터 입력
     	var data = body.boxdata;
     	console.log('boxdata insert');
     	console.log('data: ' + data[0].name);
     	console.log('data len: ' + data.length);
-    	var arr = new Array(data.length);
-    	var boxdata = {
-    		box: "true",
-    		bdata: arr
-    	}
+    	var boxData = new boxDataModel();
+    	boxData.box = "true";
+    	boxData.bdata = new Array(data.length);
+    	
     	var i = 0;
     	for(var i = 0; i < data.length; i++){
-    		var indata = {
-	    		name: data[i].name,
-	    		boxId: data[i].boxId,
-	    		cost: data[i].cost,
-	    		rangeStart: data[i].rangeStart,
-	    		rangeEnd: data[i].rangeEnd,
-	    		itemList: data[i].itemList,
-	    		nextId: data[i].nextId
-    		}
-    		boxdata.bdata[i] = indata;
-    		console.log("boxdata i: " + boxdata.bdata[i].name);
+            var idata = {
+                name: data[i].name,
+                boxId: data[i].boxId,
+                cost: data[i].cost,
+                rangeStart: data[i].rangeStart,
+                rangeEnd: data[i].rangeEnd,
+                itemList: data[i].itemList,
+                nextId: data[i].nextId
+            }
+    		boxData.bdata[i] = idata;
+    		console.log("boxData i: " + boxData.bdata[i].name);
     	}
     	//console.log("boxdata: " + JSON.stringify(boxdata));
-    	db.insert(boxdata, callback);
+    	boxData.save(callback);
     }
 }
 
 function _findPayday(where, callback){
 	where = where || {};
-	console.log("find where: " + where.email);
-	// 유저 데이터 요청, 박스 데이터 요청
-	// 파라미터만 다르면 됨 고칠 것 없음
-	db.find(where, callback);
+	// where에 주소에 넣은 파라미터 나옴
+	console.log("find where email: " + where.email);
+	console.log("find where did: " + where.did);
+	console.log("find where: " + where.box);
+	if(where.email != null && where.did != null){
+		// 유저 데이터 요청
+		userDataModel.find(where, callback);
+	}
+	else if(where.box != null){
+		// 박스 데이터 요청
+		boxDataModel.find(where, callback);
+	}
 }
 
 function _updatePayday(where, body, callback){
-	body = typeof body === 'strng' ? JSON.parse(body) : body;
+	var body = typeof body === 'strng' ? JSON.parse(body) : body;
 	console.log("update where: " + where.toString());
-	db.update(where, {$set: body}, {multi: true}, callback);
+	console.log("body: " + body.toString());
+	if(where.email != null){
+		userDataModel.findOne(where, function(err, userData){
+			if(err){
+				throw err;
+			}
+			else{
+				if(userData != null){
+					userData.money = body.money;
+					userData.rank = body.rank;
+					userData.heart = body.heart;
+					userData.charge = body.charge;
+					userData.currentBoxId = body.currentBoxId;
+					userData.pickItems = body.pickItems;
+					userData.getPush = body.getPush;
+					userData.save(callback);	
+				}
+			}
+		});
+	}
 }
 
 function _removePayday(where, callback){
-	db.remove(where, {multi: true}, callback);
+	if(where.email != null && where.did != null){
+		userDataModel.remove(where, callback);
+	}
 }
